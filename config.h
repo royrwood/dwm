3a5999830b5fd3718634bfaa89e28b63e16f3586
Roy Wood <roy@binnj.com>
2022-08-04 22:08:17 -0400

    Start tweaking keys

--- config.h
+++ config.h
@@ -60,7 +60,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+// static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "gnome-terminal", NULL };
 
 	// TAGKEYS(                        XK_1,                      0)
 	// TAGKEYS(                        XK_2,                      1)
@@ -83,19 +84,18 @@ static const char *termcmd[]  = { "st", NULL };
 	// { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	// { MODKEY,                       XK_space,  setlayout,      {0} },
 	// { MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	// { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	// { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	// { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	// { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	
+
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY|AltMask,               XK_Left,   focusmon,       { .i = -1 } },
 	{ MODKEY|AltMask,               XK_Right,  focusmon,       { .i = +1 } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_q,      quit,           { 0 } },
-	{ MODKEY,                       XK_j,      focusstack,     { .i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     { .i = -1 } },
+	{ MODKEY,                       XK_Left,   focusstack,     { .i = +1 } },
+	{ MODKEY,                       XK_Right,  focusstack,     { .i = -1 } },
 	{ MODKEY,                       XK_1,      view,           { .ui = 1 } },
 	{ MODKEY,                       XK_2,      view,           { .ui = 2 } },
 	{ MODKEY,                       XK_3,      view,           { .ui = 4 } },
